69eec28a9f90ea2bc03484c934a12e68d22818c9
Patryk Gitner <209317@edu.p.lodz.pl>
2019-06-11 14:38:33 +0200

    initial commit

---
+++ src/main/java/com/zpi/zpitest/ZpiTestApplication.java
@@ -0,0 +1,13 @@
+package com.zpi.zpitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZpiTestApplication.class, args);
+	}
+
+}
